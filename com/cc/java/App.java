32295f67edfb7044e5ff7a7e67bd6a14e821899b
Zubair-Eb <zubair.yousaf-eberl@gmx.de>
2022-04-08 12:28:03 +0200

    Wert zuweisung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,8 +5,29 @@ public class App {
     public static void main(String[] args) {
      
        Cat cat = new Cat();
+       
+       cat.name= "Grizabella";
+       cat.forColor = "White";
+       cat.age= 20;
+       output("Name: \t\t" + cat.name);
+       output("Fellfarbe: \t" + cat.forColor);
+       output("Alter: \t\t" + cat.age);
 
-       output("Blick von aussen: " +cat);
+       output("-------------------------------------------");
+
+       Cat cat1 = new Cat();
+       
+       cat1.name= "Tom";
+       cat1.forColor = "Black";
+       cat1.age= 25;
+       output("Name: \t\t" + cat1.name);
+       output("Fellfarbe: \t" + cat1.forColor);
+       output("Alter: \t\t" + cat1.age);
+
+       output("-------------------------------------------");
+
+
+ /*    output("Blick von aussen: " +cat);
        output("Blick von innen: " + cat.tellYourAddress());
        output("-------------------------------------------");
 
@@ -14,7 +35,7 @@ public class App {
        output("Blick von aussen: " +cat1);
        output("Blick von innen: " + cat1.tellYourAddress());
        output("-------------------------------------------");
-
+*/
 
 
 
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,6 +2,17 @@ package com.cc.java;
 
 public class Cat {
     
+
+    public String name;
+    public String forColor;
+    public int age;
+        
+
+
+
+
+
+
     public Cat tellYourAddress() {
         return this;
     }
